63f62d09b1a3378255c30e46573074e194746b26
Madyan.21 <madyan.m005@gmail.com>
2025-11-30 17:53:35 +0300

    Update login.dart

--- bin/login.dart
+++ bin/login.dart
@@ -1,23 +1,27 @@
 import 'dart:io';
 import 'database/users.dart';
-import 'functions.dart';
 
-void login() {
+bool login() {
   stdout.write("Please Enter your Name : ");
   String name = stdin.readLineSync()!;
 
   var found = users.where((u) => u.username == name).toList();
   if (found.isEmpty) {
-    print("The user name not found , please register.");
-    return;
+    print("""
+❌ User not found!
+👉 Please choose option 2️⃣  (Register) from the main menu to create a new account.
+""");
+
+    return false;
   } else {
     var userbud = found.first;
 
-    stdout.write("Please Enter password : ");
-    String password = stdin.readLineSync()!;
+    while (true) {
+      stdout.write("Please Enter password : ");
+      String password = stdin.readLineSync()!;
 
-    if (password == userbud.password) {
-      print("""
+      if (password == userbud.password) {
+        print("""
 ========================================
 🎉 Login successful! Welcome, $name
 ----------------------------------------
@@ -25,8 +29,10 @@ void login() {
 💰 Budget Available: \$${userbud.budget}
 ========================================
 """);
-
-      displayMenu2();
+        return true;
+      } else {
+        print("❌ Wrong password! Please try again.");
+      }
     }
   }
 }
